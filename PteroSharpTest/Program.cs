e4b9573a4d94caf4d0d07f3eb89f6ab5889ba766
Marcel Baumgartner <marcel.kbkm@gmail.com>
2021-10-30 16:48:50 +0200

    Projektdateien hinzufügen.

---
+++ PteroSharpTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PteroSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
